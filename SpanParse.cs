57a30107304f5915499fe1d7a31498e311d9afce
Matheos Mattsson <matheos.mattsson@gmail.com>
2023-11-20 00:13:46 +0200

    Initial commit

---
+++ SpanParse.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+
+namespace SpanParseForStandard
+{
+	public static class SpanParse
+	{
+		public static int ParseInt(ReadOnlySpan<char> charSpan)
+		{
+			var trimmed = charSpan.Trim();
+			var result = 0;
+			var multiplicator = 1;
+			for (var i = trimmed.Length - 1; i >= 0; i--)
+			{
+				if (i == 0 && trimmed[i] == '-')
+				{
+					result *= -1;
+					break;
+				}
+				result += multiplicator * CharToInt(trimmed[i]);
+				multiplicator *= 10;
+			}
+			return result;
+		}
+
+		public static bool TryParseInt(ReadOnlySpan<char> charSpan, out int value)
+		{
+			try
+			{
+				value = ParseInt(charSpan);
+				return true;
+			}
+			catch 
+			{
+				value = default;
+				return false; 
+			}
+		}
+
+		public static double ParseDouble(ReadOnlySpan<char> charSpan)
+		{
+			var trimmed = charSpan.Trim();
+
+			//Find decimal separator
+			var separator = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator.AsSpan();
+			var separatorStart = -1;
+			for (var i = 0; i < trimmed.Length - separator.Length; i++)
+			{
+				if (trimmed.Slice(i, separator.Length).Equals(separator, StringComparison.OrdinalIgnoreCase))
+				{
+					separatorStart = i;
+					break;
+				}
+			}
+
+			// Integer part
+			double result = ParseInt(trimmed.Slice(0, separatorStart));
+
+			// Decimal part
+			var decimalPart = trimmed.Slice(separatorStart + separator.Length);
+			result += ParseInt(decimalPart) / Math.Pow(10, decimalPart.Length);
+
+			return result;
+		}
+
+		public static bool TryParseDouble(ReadOnlySpan<char> charSpan, out double value)
+		{
+			try
+			{
+				value = ParseDouble(charSpan); 
+				return true;
+			}
+			catch
+			{
+				value = default;
+				return false;
+			}
+		}
+
+
+		private static int CharToInt(char c) => char.IsDigit(c) ? c - 48 : throw new FormatException("ReadOnlySpan<char> was in incorrect format.");
+	}
+}
